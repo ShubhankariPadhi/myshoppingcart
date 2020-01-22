4f0dca0b2fa7d5c638e2b65aecf92b87db0d5b7b
ShubhankariPadhi <https://github.com/ShubhankariPadhi/myshoppingcart.git>
2020-01-22 19:43:25 +0530

    "applied grid "

--- get_products.js
+++ get_products.js
@@ -1,89 +1,98 @@
 
 
 $(document).ready(function () {
-
+ /*declaring  arrays of subTypes*/
     var mobile=new Array(Object);
     var biography=new Array(Object);
     var fiction=new Array(Object);
     var comic=new Array(Object);
 
+    /*declaring variables*/
     var mob=0,fic=0,bio=0,com=0;
 
     var empdata='' ;
-    // var ids='';
     var ratings=0;
     var n=0;
     var subType_prod='';
-    var m='';
-    $.getJSON("products.json",function(data){
 
-        //console.log(data.length);
+   /*////////// fetching json data and store them in arrays of subtypes & append them on events call //////////*/
+    $.getJSON("products.json",function(data){
         n=data.length;
 
         for(var i=0;i<n;i++){
-
             ratings=data[i].rating;
             subType_prod=data[i].subType;
 
-            /*  ///////////////////// home page with highly rated products  ////////////////////////////*/
-
-
+            /*  appending json data to home page with highly rated products */
                 if(ratings>=4){
-                empdata += "<ul type='none'><li>"+"<img style='height: 100px;width: 70px' src='"+data[i].imgPath+"'>"+"</li>";
+                empdata += "<ul type='none'><li>"+"<img style='height: 300px;width: 200px' src='"+data[i].imgPath+"'>"+"</li>";
                 empdata += "<li>" + data[i].name + "</li>";
-                empdata += "<li> <span>price </span>" + data[i].price + "</li><li>"+ "<span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span> "+  "</li></ul>";
+                empdata += "<li> <span><i class='fa fa-inr' aria-hidden='true'></i> </span>" + data[i].price + "</li><li>"+ "<span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span> "+  "</li></ul>";
+                }
 
+            /* storing electronic type products from json obj into arrays of its corresponding subTypes*/
 
-
-            }
-
-            /* ///////////////////////////     electronics arrays    ///////////////////////////*/
-            m=subType_prod.localeCompare("mobile");
-            if( m==0){
-                mobile[mob] = "<ul type='none'><li>" + "<img style='height: 100px;width: 70px' src='" + data[i].imgPath + "'>" + "</li>"
-                             + "<li>" + data[i].name + "</li>" + "<li> <span>price </span> " + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span> "+"</li></ul>";
+            if( subType_prod.localeCompare("mobile")==0){
+                mobile[mob] = "<ul type='none'><li>" + "<img style='height: 300px;width: 200px' src='" + data[i].imgPath + "'>" + "</li>"
+                             + "<li>" + data[i].name + "</li>" + "<li> <span><i class='fa fa-inr' aria-hidden='true'></i> </span> " + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span> "+"</li></ul>";
                 mob++;
             }
 
-            /*  /////////////////////////////////  book arrays //////////////////////  */
+            /*  storing book type products from json obj into  arrays of its corresponding subTypes */
 
-            m=subType_prod.localeCompare("biography");
-            if(m==0){
-                biography[bio] = "<ul type='none'><li>" + "<img style='height: 100px;width: 70px' src='" + data[i].imgPath + "'>" + "</li>"
-                    + "<li>" + data[i].name + "</li>" + "<li> <span>price </span>" + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span>"+ "</li></ul>";
+            if(subType_prod.localeCompare("biography")==0){
+                biography[bio] = "<ul type='none'><li>" + "<img style='height: 300px;width: 200px' src='" + data[i].imgPath + "'>" + "</li>"
+                    + "<li>" + data[i].name + "</li>" + "<li> <span><i class='fa fa-inr' aria-hidden='true'></i> </span>" + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span>"+ "</li></ul>";
                 bio++;
             }
-
-
-            m=subType_prod.localeCompare("fiction");
-            if(m==0){
-                fiction[fic] = "<ul type='none'><li>" + "<img style='height: 100px;width: 70px' src='" + data[i].imgPath + "'>" + "</li>"
-                    + "<li>" + data[i].name + "</li>" + "<li> <span>price </span>" + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span>" +"</li></ul>";
+            if(subType_prod.localeCompare("fiction")==0){
+                fiction[fic] = "<ul type='none'><li>" + "<img style='height: 300px;width: 200px' src='" + data[i].imgPath + "'>" + "</li>"
+                    + "<li>" + data[i].name + "</li>" + "<li> <span><i class='fa fa-inr' aria-hidden='true'></i> </span>" + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span>" +"</li></ul>";
                 fic++;
             }
-
-            m=subType_prod.localeCompare("comic");
-            if(m==0){
-                comic[com] = "<ul type='none'><li>" + "<img style='height: 100px;width: 70px' src='" + data[i].imgPath + "'>" + "</li>"
-                    + "<li>" + data[i].name + "</li>" + "<li> <span>price </span>" + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span>"+ "</li></ul>";
+            if(subType_prod.localeCompare("comic")==0){
+                comic[com] = "<ul type='none'><li>" + "<img style='height: 300px;width: 200px' src='" + data[i].imgPath + "'>" + "</li>"
+                    + "<li>" + data[i].name + "</li>" + "<li> <span><i class='fa fa-inr' aria-hidden='true'></i> </span>" + data[i].price +"</li><li><span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span>"+ "</li></ul>";
                 com++;
             }
 
         }
 
 
-        $("#emp").append(empdata);
+        /*  appending highly rated  products to home page  */
 
+         $("#emp").append(empdata);
 
+        /* appending subtype arrays on hover on types of products */
 
-      /* ////////////////////       home page         ///////////////////////////////*/
+        $("#elec_tab").hover(function () {
+            $("#emp").empty();
+            $("#emp").append(mobile);
+        });
+        $("#books_tab").hover(function () {
+
+            $("#emp").empty();
+            $("#emp").append(fiction);
+            $("#emp").append(biography);
+        });
+        $('#navbarDropdownElectronics').mouseenter(function () {
+            $('#electronics_dropdown').toggle('medium');
+            $('#electronics_dropdown').hide();
+        });
+
+        $('#navbarDropdownBooks').mouseenter(function () {
+            $('#showbooks').toggle('medium');
+            $('#showbooks').hide();
+        });
+
+      /* appending data to home page on home click button*/
 
         $("#home_id").click(function () {
             $("#emp").empty();
             $("#emp").append(empdata);
         });
 
-        /* //////////////////////        electronics      ///////////////////////////*/
+        /* appending electronic subtypes on clicking the particular subtypes */
 
         $("#mobile_id").click(function () {
             $("#emp").empty();
@@ -91,12 +100,9 @@ $(document).ready(function () {
                 $("#emp").append(mobile[i]);
             }
 
-
-
         });
 
-
-        /* ////////////////////////      books     ///////////////////////////////////*/
+        /*appending book subtypes on clicking the particular subtypes */
 
         $("#biography_id").click(function () {
             $("#emp").empty();
@@ -114,26 +120,10 @@ $(document).ready(function () {
         });
 
 
+
+
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
