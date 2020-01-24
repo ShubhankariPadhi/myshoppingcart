c260b5aab114eb850e99eef090b2bef7ee179ad6
ShubhankariPadhi <https://github.com/ShubhankariPadhi/myshoppingcart.git>
2020-01-24 19:25:04 +0530

    " displaying details of the product on clicking on it"

--- test.js
+++ test.js
@@ -35,11 +35,10 @@ $(document).ready(function () {
             subType_prod=data[i].subType;
             ids=data[i].id;
 
-      arr[a]+=data[i].id;
-           a++;
 
            arr_disp[i] = "<ul  id='"+data[i].id+"' type='none'><li>"+"<img class='hom' id='"+data[i].id+"'  style='height: 300px;width: 200px' src='"+data[i].imgPath+"'>"+"</li>"
                         +"<li>" + data[i].name + "</li>"+
+
                     "<li> <span><i class='fa fa-inr' aria-hidden='true'></i> </span>" + data[i].price + "</li><li>"+ "<span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span> "+  "</li></ul>";
 
 
@@ -89,45 +88,164 @@ $(document).ready(function () {
 
 
 
-     /* fetching product details on clicking the image    */
-    $(document).on('click', '.hom', function () {
-       // console.log(this);
-        $("#emp").empty();
-        $("#emp").append(this);
-       // console.log(this.id);
-
-        for(var i=0;i<data.length;i++){
-           // console.log(data[i].value);
-            if(this.id==data[i].id){
-                $("#emp").append(
-                    "<div col-sm-6>" +"<ul type='none' class='hom' type='none'>"+
-                    " <li>"+data[i].id+" </li>"+
-                    " <li>"+data[i].name+" </li>"+
-                    "<li> <span><i class='fa fa-inr' aria-hidden='true'></i> </span>" + data[i].price + "</li>"+
-                    +"<li>"+ "<span class='btn btn-success'>"+data[i].rating+"<i class='fa fa-star' aria-hidden='true'></i></span> "+
-                   " <li>"+data[i].description+" </li>"+
-                "<li>"+data[i].type +"</li>"+
-                +"<li>"+data[i].subType+"</li>"+
-                +"<li>"+data[i].brand +"</li>"+
-                +"<li>"+data[i].RAM+"</li>"+
-                +"<li>"+data[i].modelName +"</li>"+
-                +"<li>"+data[i].color +"</li>"+
-                +  " <li></li></ul></div>");
-               // console.log(data[i].brand);
-            }
-        }
-
-
-
-    });
-
-
 
 
 
         /*  appending highly rated  products to home page  */
 
         $("#emp").append(high_ratings);
+
+
+        /* fetching product details on clicking the image    */
+        $(document).on('click', '.hom', function () {
+            // console.log(this);
+            $("#emp").empty();
+            //$("#emp").append(this);
+            // console.log(this.id);
+
+            for(var i=0;i<data.length;i++) {
+                // console.log(data[i].value);
+                var ofr_type;
+                var ofr_amt;
+                var cmnt_text,cmnt_rating,cmnt_commentedOn,cmnt_username;
+                var tot_rating_product=0;
+                var tot_reviews_product=0;
+
+                if (this.id == data[i].id) {
+
+
+
+
+
+                    $("#emp").append(
+                        "<div class='col-sm-5'><img style='height: 500px;width: 450px' src='"+data[i].imgPath+"' >" +
+                        "<br><div ><button class='btn  col-sm-5' style='background-color: orange'>Add to cart</button> "+
+                        "<button class='btn  col-sm-6' style='background-color: tomato'>Add to Buy</button>"+
+                        "</div>"+
+                        "</div>"+
+                        "<div id='new' class='col-sm-7'>" + "<ul type='none'   class='hom' type='none'>" +
+                        " <li><b> " + data[i].name + "</b></li>" +
+                        "<li class='append_tot_rating_reviews'>" + "<span class='btn btn-success'>" + data[i].rating + "<i class='fa fa-star' aria-hidden='true'></i></span> </li> " +
+                        "<li class='text-success'> Special price</li>"+
+                        "<li id='offers_append'> <i class='fa fa-inr' aria-hidden='true'></i><span id='ofr_price'> " + data[i].price + " </span> </li>" +
+                        "</ul></div> "
+                        );
+
+
+                    for(var j=0;j<data[i].offers.length;j++){
+                        ofr_type=data[i].offers[j].type;
+                        ofr_amt=data[i].offers[j].amount;
+                        var old_amt=data[i].price;
+                        if(data[i].offers[j].type=="Book now"){
+
+                                 data[i].price= old_amt-ofr_amt;
+                                 $("#ofr_price").empty();
+                                $("#ofr_price").append(
+                                "<span> "+ data[i].price +" <del>"+old_amt+"  </del></span>"
+                              );
+
+                             }
+
+                        if(data[i].offers[j].type=="Emi"){
+
+                            $("#ofr_price").append(
+                                "<span>Emi offer price is "+data[i].offers[j].amount+"  </span>"
+                            );
+
+                        }
+
+
+
+
+
+                       $('#new').append(
+                           "<ul type='none' class='text-success'><h5>Available offers </h5>" +
+                           "<li class='text-success'>Get flat " +  ofr_amt+ " <span>off on </span>" + ofr_type + " </li></ul>"
+                       );
+                      //  console.log(data[i].offers[j].type)
+                    }//offers
+
+
+                    $("#new").append("<ul type='none'>" +
+                        " <li class=''><b>Description:</b>  " + data[i].description + " </li>" +
+                        "</ul>");
+
+
+
+                    // console.log(data[i].brand);
+
+
+                    if (data[i].subType == "mobile") {
+                        $("#new").append("<ul type='none'><h5>Specifications</h5>" +
+                            "<li><b>Brand </b> " + data[i].brand + "  </li>" +
+                            "<li><b>Device Type </b> " + data[i].type + "</li>" +
+
+                            "<h5>Display Features</h5> "+
+                            "<li> <b>browse type </b>" + data[i].subType + "</li>" +
+                            "<li><b>Model Name</b>  " + data[i].modelName + "</li>" +
+                            "<li><b>Color:</b> " + data[i].color + "</li>"+
+
+                            "<h5>Memory & Storage Features</h5> "+
+                            "<li><b>RAM </b> " + data[i].RAM + "</li>" +
+
+                            "<h5 id='cam'>Camera Features</h5>"+
+                            "<li> <b>Front </b> "+data[i].camera.front+ "</li>"+
+                            "<li> <b>Rear </b>"+data[i].camera.rear+ "</li>"
+                           +"</ul>"
+                        );
+                    }//mobile  specifications
+
+
+                    if (data[i].type == "books") {
+                        $("#new").append("<ul type='none'><h5>Specifications</h5>" +
+                            "<li><b>Book Type: </b> " + data[i].subType + "</li>" +
+                            "<li><b>Author :</b> " + data[i].by + "</li> </ul>"
+                        );
+                    }//books specifications
+
+
+                    //if (data[i].type == "comments") {
+                        $('#new').append("<ul type='none'>"+
+                            "<li > <b class='col-sm-4'>Ratings & Reviews </b>" + "<span class='offset-2 btn btn-success  '>" + data[i].rating + "<i class='fa fa-star' aria-hidden='true'></i> " +
+                            "</span><span class='col-sm-4 append_tot_rating_reviews '></span></li></ul>" );
+
+                        for(var j=0;j<data[i].comments.length;j++){
+                            if(data[i].comments[j].rating){
+                                tot_rating_product++;
+                            }
+                            if(data[i].comments[j].text){
+                                tot_reviews_product++;
+                            }
+
+                            cmnt_text=data[i].comments[j].text;
+                            cmnt_rating=data[i].comments[j].rating;
+                            cmnt_commentedOn=data[i].comments[j].commentedOn;
+                            cmnt_username=data[i].comments[j].username;
+
+                            $('#new').append("<ul type='none'> "+
+                                "<li>" + "<span class='btn  btn-success'> " + cmnt_rating+ " <i class='fa fa-star' aria-hidden='true'></i></span><span>  "+ cmnt_text+" </span> </li> " +
+
+                                "<li class='text-secondary'>"+ cmnt_username+" <span> <i class='fa fa-check-circle' aria-hidden='true'></i> Certified Buyer,location , on </span>"+cmnt_commentedOn+"</li>"
+
+                            );
+
+                        }
+$(".append_tot_rating_reviews").append("<span>  " +
+    tot_rating_product +" Ratings & "+tot_reviews_product+ " Reviews </span>");
+
+                   // }//comments
+
+
+
+
+
+
+
+                }//if data[i] closed
+
+            }//for closed
+
+        });// on close
 
 
 
